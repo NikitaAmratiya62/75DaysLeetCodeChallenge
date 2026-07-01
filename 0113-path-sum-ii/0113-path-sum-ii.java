80776c828b9f5a76e1e22197e889da8c15b802f2
NikitaAmratiya62 <nikitapatidar084@gmail.com>
2026-07-01 14:26:14 +0530

    Time: 1 ms (99.97%), Space: 45.3 MB (72.76%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void dfs(TreeNode root, int targetSum,List<List<Integer>> ans,List<Integer> path) {
+        if(root==null) return;
+        path.add(root.val);
+        if(root.left==null && root.right==null && targetSum==root.val){
+            ans.add(new ArrayList<>(path));
+        }
+        else{
+            dfs(root.left,targetSum-root.val,ans,path);
+            dfs(root.right,targetSum-root.val,ans,path);
+        }
+        path.remove(path.size()-1);
+        
+
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> path=new ArrayList<>();
+        dfs(root,targetSum,ans,path);
+        return ans;
+
+    }
+}
